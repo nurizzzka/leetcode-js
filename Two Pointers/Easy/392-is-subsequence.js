4aaa411964b3e2181e53519334eaf13c015104d4
Nuriza Akynbaeva <39913575+nurizzzka@users.noreply.github.com>
2026-09-03 11:11:30 +0600

    Update: 392. Is Subsequence (JavaScript)

--- Two Pointers/Easy/392-is-subsequence.js
+++ Two Pointers/Easy/392-is-subsequence.js
@@ -3,7 +3,7 @@
 // Difficulty: Easy
 // Topics: Two Pointers, String, Dynamic Programming
 // Runtime: 3 ms (Beats 17.9%)
-// Memory: 55.6 MB (Beats 50.6%)
+// Memory: 55.8 MB (Beats 41.3%)
 // Submitted: Sep 3, 2026
 // Link: https://leetcode.com/problems/is-subsequence/
 // ═══════════════════════════════════════════════════════
@@ -16,16 +16,11 @@
 var isSubsequence = function(s, t) {
     let s1 = 0;
     let t1 = 0;
-    let res = '';
     while (s1 < s.length && t1 < t.length) {
-        if (s[s1] !== t[t1]) {
-          t1++;
+        if (s[s1] === t[t1]) {
+          s1++;
         }
-        else {
-            res += s[s1];
-            s1++;
-            t1++;
-        }
+        t1++;
     }
-    return res === s ? true : false;
+    return s1 === s.length;
 };
